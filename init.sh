b8b263d973ca00b0fbd9385c2747718510c651ec
Jeremy A. Puhlman <jpuhlman@mvista.com>
2019-11-06 01:29:15 -0800

    Fix docker permissions

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ groupadd -g $USERGID engr
 useradd -s /bin/bash -d $HOME -M -u $USERID -g $USERGID $USERNAME
 echo "$USERNAME  ALL=(ALL)       NOPASSWD: ALL" | tee -a /etc/sudoers > /dev/null
 sudo usermod -a -G mock $USERNAME
+chown root.engr /var/run/docker.sock
 echo "export MOCK_OPTS='--old-chroot'" >> /etc/bashrc
 echo
 echo "Welcome to ubuntu 18.04 builder"
